0eba4caa054243ebde7ff540d941042034c9a96d
Ivan Martinez <ivanmartinez.gonzalez97@gmail.com>
2025-12-29 16:11:59 -0600

    Fixing header content full width

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -14,7 +14,7 @@ function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center justify-between px-4">
+      <div className="flex h-14 items-center justify-between px-4 w-full">
         <div className="mr-4 hidden md:flex">
           <a className="mr-6 flex items-center space-x-2" href="/">
             <span className="hidden font-bold sm:inline-block">
